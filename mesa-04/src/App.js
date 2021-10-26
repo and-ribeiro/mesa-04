b3ecdadce5647b7aae26508c4f5948bea5b7abcc
andersonsilvaribeiro <andersonsiilvar@gmail.com>
2021-10-26 10:09:42 -0300

    Adicionando Mesa de Trabalho

---
+++ mesa-04/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">       
+        <a>Mesa de Trabalho 04</a>
+        <img src="homem-feliz-dinheiro.jpg"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
